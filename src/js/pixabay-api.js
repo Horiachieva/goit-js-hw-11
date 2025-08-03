7bf3e0c5810f26ae3e6479ce160267c43c892d4e
Myroslava <m6i6y6a@gmail.com>
2025-08-03 20:22:50 +0200

    add htm, css, js

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '51402462-7ac40c84763508a553a5c1d6d';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
